fdbcd0b22321db2db044e846364f1021ab7bb6f6
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-08-04 20:10:05 +0000

    Up to lesson 31.

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
-    
+def index():
+    return url_for('show_user_profile',username='brian')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        return 'username is: ' + request.values['username']
+    else:
+        return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
+
+
 if __name__ == '__main__':
     host = os.getenv('IP','0.0.0.0')
     port = int(os.getenv('PORT',5000))
+    app.debug = True
     app.run(host=host,port=port)
