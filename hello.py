7ecb2a18d17e8bde8d352e03bbb6c3c15406dde4
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-08-12 21:01:42 +0000

    Updated base templates, etc.

--- hello.py
+++ hello.py
@@ -1,22 +1,38 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for, redirect, flash
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return url_for('show_user_profile',username='brian')
 
-
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=["GET","POST"])
 def login():
+    error = None
     if request.method == 'POST':
-        return 'username is: ' + request.values['username']
+        if valid_login(request.form['username'],request.form['password']):
+            flash("Successfully logged in.")
+            # return "Welcome back, {}".format(request.form["username"])
+            return redirect(url_for('welcome',username=request.form.get('username')))
+        else:
+            error = "Incorrect username and password."
+    return render_template('login.html', error=error)
+        
+def valid_login(username,password):
+    if username == password:
+        return True
     else:
-        return '<form method="post" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
+        return False
+        
+@app.route('/welcome/<username>')
+def welcome(username):
+    return render_template('welcome.html')
 
 
 if __name__ == '__main__':
     host = os.getenv('IP','0.0.0.0')
     port = int(os.getenv('PORT',5000))
     app.debug = True
+    app.secret_key = 'SuperSecret'
     app.run(host=host,port=port)
+    
